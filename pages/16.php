c63fc9a34ce5180dacac6078c846396e2063294a
Sven Waser <sven.waser@brogrammer.ch>
2021-05-13 22:16:03 +0200

    Changes on checkout.php / all main functions created

--- pages/16.php
+++ pages/16.php
@@ -2,5 +2,10 @@
 $checkout = new Checkout();
 $checkout->cashier = 1;
 
-var_dump($checkout->list());
+var_dump($checkout->transactions());
+
+$checkout->add( CHECKOUT::DEFAULT_TABLE , array(
+  "checkout_id" => "TEST",
+  "name" => "hello World",
+));
 ?>
--- php/checkout.php
+++ php/checkout.php
@@ -17,7 +17,19 @@
  * Variables witch have to be passd through the function are written after the function name inround brackets ().
  * All functions can be used as Static
  *
- * Checkout->list ( $offset [int], $steps [int] ) [$cashier]
+ * Checkout->transactions ( $offset [int], $steps [int] ) [$cashier]
+ *
+ * Checkout->add ( $table [const], $values [array] ) [$cashier]
+ *
+ * Checkout->update_checkout ( $values [array] ) [$cashier]
+ *
+ * Checkout->update_product( $product_id [int], $values [$array] ) [$cashier]
+ *
+ * Checkout->remove_checkout () [$cashier]
+ *
+ * Checkout->remove_product ( $product_id [int] ) [$cashier]
+ *
+ * Checkout->remove_access ( $user [int] ) [$cashier]
  *
  * Checkout->access ( $user [int or null] ) [$cashier]
  *
@@ -33,6 +45,11 @@ class Checkout {
   //Variables
   public $cashier;
 
+  //constants
+  const DEFAULT_TABLE = CHECKOUT;
+  const PRODUCTS_TABLE = CHECKOUT_PRODUCTS;
+  const ACCESS_TALBE = CHECKOUT_ACCESS;
+
   /**
    * Returns a list of all transactions (in steps) that belong to the checkout
    * requires: $cashier
@@ -43,7 +60,7 @@ class Checkout {
    * Important notice. This function is recursive so please use the $steps wisely and do not execute this function with a very heigh $steps number
    * The offset you can set as heigh as you want this does not affect the function in executiont ime
    */
-  public function list( $offset = 0, $steps = 20) {
+  public function transactions( $offset = 0, $steps = 20) {
     // Define transaction list
     $transaction_list = array();
 
@@ -113,6 +130,162 @@ class Checkout {
     // All elements found
     return $transaction_list;
   }
+
+  /**
+   * Adds a new row in requested table
+   *
+   * $table: Uses Table name use class constants (Supports DEFAULT_TABLE and PRODUCTS_TABLE)
+   * $values: Array with new values
+   *          DFAULT TABLE:   array(
+   *                            name,
+   *                            payment_payrexx_instance,
+   *                            payment_payrexx_secret
+   *                          )
+   *          PRODUCTS_TABLE: array(
+   *                            checkout_id,
+   *                            name,
+   *                            price,
+   *                            currency
+   *                          )
+   *          ACCESS_TABLE:   array(
+   *                            checkout_id,
+   *                            user_id
+   *                          )
+   */
+  public function add( $table = SELF::DEFAULT_TABLE, $values ) {
+    //Get database connection
+    $conn = Access::connect();
+
+    //Generate query
+    $add_query = "INSERT INTO " . $table . " ";
+    $add_query .= "(" . implode(", ", array_flip($values)) . ") ";
+    $add_query .= "VALUES ('" . implode("', '", $values) . "')";
+
+    // execute query
+    $add = $conn->prepare($add_query);
+    return $add->execute();
+  }
+
+  /**
+   * Updates a checkout
+   * requires: $cashier
+   *
+   * $values: Array with new values
+   *          array(
+   *            name,
+   *            payment_payrexx_instance,
+   *            payment_payrexx_secret
+   *         )
+   */
+  public function update_checkout( $values) {
+    //Get database connection
+    $conn = Access::connect();
+
+    // Check values
+    $valid_values = array("name", "payment_payrexx_instance", "payment_payrexx_secret");
+    $checked_values = array_intersect_key($values, array_flip($valid_values));
+
+    // Generate values and keys
+    $update_query = "UPDATE " . CHECKOUT . " SET ";
+    foreach( $checked_values as $key => $value ) {
+      $update_query .= "'" . $key . "' = '" . $value . "', ";
+    }
+    $update_query = substr( $update_query, 0, -2 ) . " WHERE checkout_id=:checkout_id";
+
+    // Update query
+    $update = $conn->prepare( $update_query );
+    return $update->execute(array(
+      ":checkout_id" => $this->cashier,
+    ));
+  }
+
+  /**
+   * Updates a product
+   * requires: $cashier
+   *
+   * $product_id: Id of product (stored in database)
+   * $values: Array with new values
+   *          array(
+   *            checkout_id,
+   *            name,
+   *            price,
+   *            currency
+   *         )
+   */
+  public function update_product( $product_id, $values ) {
+    //Get database connection
+    $conn = Access::connect();
+
+    // Check values
+    $valid_values = array("name", "price", "currency");
+    $checked_values = array_intersect_key($values, array_flip($valid_values));
+
+    // Generate values and keys
+    $update_query = "UPDATE " . CHECKOUT_PRODUCTS . " SET ";
+    foreach( $checked_values as $key => $value ) {
+      $update_query .= "'" . $key . "' = '" . $value . "', ";
+    }
+    $update_query = substr( $update_query, 0, -2 ) . " WHERE checkout_id=:checkout_id AND id=:id";
+
+    // Update query
+    $update = $conn->prepare( $update_query );
+    return $update->execute(array(
+      ":checkout_id" => $this->cashier,
+      ":id" => $product_id,
+    ));
+  }
+
+  /**
+   * Removes a checkout
+   * requires: $cashier
+   */
+  public function remove_checkout() {
+    //Get database connection
+    $conn = Access::connect();
+
+    // Remove
+    $remove = $conn->prepare("DELETE FROM " . CHECKOUT . " WHERE checkout_id=:checkout_id");
+    return $remove->execute(array(
+      ":checkout_id" => $this->cashier,
+    ));
+  }
+
+  /**
+   * Removes a product
+   * requires: $cashier
+   *
+   * $product_id: Id of product (stored in database)
+   */
+  public function remove_product( $product_id ) {
+    //Get database connection
+    $conn = Access::connect();
+
+    // Remove
+    $remove = $conn->prepare("DELETE FROM " . CHECKOUT_PRODUCTS . " WHERE checkout_id=:checkout_id AND id=:id");
+    return $remove->execute(array(
+      ":checkout_id" => $this->cashier,
+      ":id" => $product_id,
+    ));
+  }
+
+  /**
+   * Removes an access
+   * requires: $cashier
+   *
+   * $user: User id (stored in database)
+   */
+  public function remove_access( $user ) {
+    //Get database connection
+    $conn = Access::connect();
+
+    // Remove
+    $remove = $conn->prepare("DELETE FROM " . CHECKOUT_ACCESS . " WHERE checkout_id=:checkout_id AND user_id=:user_id");
+    return $remove->execute(array(
+      ":checkout_id" => $this->cashier,
+      ":user_id" => $user,
+    ));
+  }
+
 
   /**
    * Returns true or false or if user is equal to null it returns list of users who have access to the checkout
@@ -185,9 +358,9 @@ class Checkout {
     $conn = Access::connect();
 
     // Get all values from checkout
-    $checkout = $conn->prepare("SELECT * FROM " . CHECKOUT . " WHERE id=:id");
+    $checkout = $conn->prepare("SELECT * FROM " . CHECKOUT . " WHERE checkout_id=:checkout_id");
     $checkout->execute(array(
-      ":id" => $this->cashier,
+      ":checkout_id" => $this->cashier,
     ));
 
     // Combine all values
