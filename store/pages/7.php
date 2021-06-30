41c9ee17f528add4730c87e510ecb5dcebb9752b
Svenwas3f <sven.waser@venusnet.ch>
2021-06-30 18:10:10 +0200

    Pubs included

--- store/pages/7.php
+++ store/pages/7.php
@@ -4,9 +4,9 @@
   </div>
 </header>
 
-<form action="" method="post"  class="search_bar">
+<form action="" method="get"  class="search_bar">
   <label>
-    <input type="text" name="search" placeholder="Nach Wirtschaft suchen"/>
+    <input type="text" name="s" placeholder="Nach Wirtschaft suchen" value="<?php echo $_GET["s"] ?? null; ?>"/>
     <button class="icon"><img src="<?php echo $url; ?>medias/icons/magnifying-glass.svg"></button>
   </label>
 </form>
@@ -20,10 +20,10 @@
 
   // List pubs
   foreach($pubs as $pub) {
-    echo '<a href="' . $url . 'pubs/menu/?pub=' . $pub["pub_id"] . '">';
+    echo '<a href="' . $url . 'store/' . $type . '/menu/' . $pub["pub_id"] . '">';
       echo '<div class="store-group-box">';
         if( empty($pub["logo_fileID"]) ) {
-          echo '<img  src="' . $url . 'medias/pubs/favicon-color-512.png"/>';
+          echo '<img  src="' . $url . 'medias/store/favicon-color-512.png"/>';
         }else {
           echo '<img  src="' . MediaHub::getUrl( $pub["logo_fileID"] ) .'"/>';
         }
@@ -38,13 +38,13 @@
 // Next/last page
 if( (count(Pub::all(($offset + $steps), 1, ($_GET["s"] ?? null))) > 0) && (($offset/$steps) > 0) ) {
   echo '<div class="page-nav">';
-    echo '<a a href="' . $url . $type . '/?row-start=' . ($offset/$steps - 1) . '" class="left" title="Vorherige Wirtschaften ansehen"><img src="' . $url . 'medias/store/icons/page-back.svg"</a>';
+    echo '<a a href="' . $url . 'store/' . $type . '/?row-start=' . ($offset/$steps - 1) . ( isset($_GET["s"]) ? "&s=" . urlencode($_GET["s"]) : "" ) . '" class="left" title="Vorherige Wirtschaften ansehen"><img src="' . $url . 'medias/store/icons/page-back.svg"</a>';
     echo '<a class="center"></a>';
-    echo '<a a href="' . $url . $type . '/?row-start=' . ($offset/$steps + 1) . '" class="right" title="Weitere Wirtschaften ansehen"><img src="' . $url . 'medias/store/icons/page-next.svg"</a>';
+    echo '<a a href="' . $url . 'store/' . $type . '/?row-start=' . ($offset/$steps + 1) . ( isset($_GET["s"]) ? "&s=" . urlencode($_GET["s"]) : "" ) . '" class="right" title="Weitere Wirtschaften ansehen"><img src="' . $url . 'medias/store/icons/page-next.svg"</a>';
   echo '</div>';
 }elseif( ($offset/$steps) > 0 ) { // Last page
   echo '<div class="page-nav">';
-    echo '<a a href="' . $url . $type . '/?row-start=' . ($offset/$steps - 1) . '" class="left" title="Vorherige Wirtschaften ansehen"><img src="' . $url . 'medias/store/icons/page-back.svg"</a>';
+    echo '<a a href="' . $url . 'store/' . $type . '/?row-start=' . ($offset/$steps - 1) . ( isset($_GET["s"]) ? "&s=" . urlencode($_GET["s"]) : "" ) . '" class="left" title="Vorherige Wirtschaften ansehen"><img src="' . $url . 'medias/store/icons/page-back.svg"</a>';
     echo '<a class="center"></a>';
     echo '<a class="right"></a>';
   echo '</div>';
@@ -52,7 +52,7 @@ if( (count(Pub::all(($offset + $steps), 1, ($_GET["s"] ?? null))) > 0) && (($off
   echo '<div class="page-nav">';
     echo '<a class="left"></a>';
     echo '<a class="center"></a>';
-    echo '<a a href="' . $url . $type . '/?row-start=' . ($offset/$steps + 1) . '" class="right" title="Weitere Wirtschaften ansehen"><img src="' . $url . 'medias/store/icons/page-next.svg"</a>';
+    echo '<a a href="' . $url . 'store/' . $type . '/?row-start=' . ($offset/$steps + 1) . ( isset($_GET["s"]) ? "&s=" . urlencode($_GET["s"]) : "" ) . '" class="right" title="Weitere Wirtschaften ansehen"><img src="' . $url . 'medias/store/icons/page-next.svg"</a>';
   echo '</div>';
 }
  ?>
